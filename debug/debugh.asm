9be93f9a76acacc3947306a92e4a3bf1c031274b
Frederic Stark <frederic.stark@gmail.com>
2024-12-12 21:53:02 +0100

    First 2 phase test

---
+++ debug/debugh.asm
@@ -0,0 +1,13 @@
+; Debug ROM, Hig-memory version
+
+; This is assembled to go at address C000
+; It is stored in the ROM at address 0400
+
+	ORG 0C000H
+
+	MVI A,'!'
+	OUT 9FH
+DONE:
+	JMP DONE
+
+	END
